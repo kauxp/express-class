2cfe40375ed79057d6dcf176fc848dc2c69847e2
kauxp <paramjeetkaur4326@gmail.com>
2024-07-03 09:46:36 +0530

    express-app

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+let courses = [
+    { id: 1, name: "java"},
+    { id: 2, name: "node"},
+    { id: 3, name: "react"}
+];
+
+app.get('/courses', (req, res)=>{
+    res.json(courses);
+}).listen(3000, 'localhost')
